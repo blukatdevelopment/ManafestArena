96a053905d303bf79b232a28aad41f558eec9fd9
blukat <blukatstudios@gmail.com>
2019-08-03 15:14:56 -0400

    feat(career): Added new object to store save data into a single file.

--- src/Db/CareerDb.cs
+++ src/Db/CareerDb.cs
@@ -8,27 +8,15 @@ using System.Text;
 using System.Collections.Generic;
 
 public class CareerDb {
-  const string StatsFile = "Saves/stats.csv";
-  const string CareerFile = "Saves/career_tree.csv";
+  public const string DefaultCareerSave = "Saves/Career.json";
+  public const string StatsFile = "Saves/stats.csv";
+  public const string CareerFile = "Saves/career_tree.csv";
 
 
   public static bool CareerExists(){
     return false;
   }
 
-  public static Career LoadCareer(){
-    //GD.Print("CareerDb.LoadCareer");
-    // List<CareerNode> nodes = CareerNode.FromRows(CSV.ReadRows(CareerFile));
-    // StatsManager stats = StatsManager.FromRows(CSV.ReadRows(StatsFile));
-    // return Career.Factory(nodes, stats);
-    return null;
-  }
-
-  public static void SaveCareer(Career career){
-    //GD.Print("CareerDb.SaveCareer");
-    //CSV.WriteToFile(StatsFile, career.stats.GetRows());
-    //CSV.WriteToFile(CareerFile, CareerNode.ToRows(career.careerNodes));
-  }
 
   public static void ClearCareer(){
     GD.Print("CareerDb.ClearCareer");
@@ -43,18 +31,28 @@ public class CareerDb {
     return false;
   }
 
-  public static PressEvent LoadPressEvent(string name){
-    return null;
-    //return new PressEvent(CSV.ReadRows(name));
-  }
-
   public static List<string> PressEventFiles(){
     string[] filePaths = System.IO.Directory.GetFiles("PressEvents/", "*.csv", SearchOption.TopDirectoryOnly);
     return new List<string>(filePaths);
   }
 
-  public static List<string> Arenas(){
-    return new List<string>{"Arena1", "Arena2", "Arena3", "Arena4"};
+  public static void SaveCareerData(CareerData data) {
+    if(data.fileName == ""){
+      data.fileName = DefaultCareerSave;
+    }
+
+    string json = Util.ToJson(data);
+
+    System.IO.File.WriteAllText(data.fileName, json);
+  }
+
+  public static CareerData LoadCareerData(string filePath = DefaultCareerSave){
+    if(!System.IO.File.Exists(filePath)){
+      return null;
+    }
+
+    string json = System.IO.File.ReadAllText(filePath);
+    return Util.FromJson<CareerData>(json);
   }
 
 }
