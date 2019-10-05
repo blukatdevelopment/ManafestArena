916bea7694ff06077a6cf90a856d59fb4b018c68
blukat <blukatstudios@gmail.com>
2019-10-04 23:05:36 -0400

    feat(sound): Moved songs config into JSON config files.

--- src/Util/Speaker.cs
+++ src/Util/Speaker.cs
@@ -8,15 +8,10 @@ using System.Collections.Generic;
 
 public class Speaker : AudioStreamPlayer3D{
   
-  public void LoadEffect(Sound.Effects effect){
-    string soundFile = Sound.EffectFile(effect);
-    this.Stream = (AudioStreamSample)GD.Load(soundFile);
-  }
-  
   public void PlayEffect(Sound.Effects effect){
     this.Stop();
     MaxDb = Sound.VolumeMath(Session.session.sfxVolume);
-    LoadEffect(effect);
+    this.Stream = Sound.LoadEffect(effect);
     this.Play();
   }
   
