504e4b27352adcd95573c9a843037935d31d8005
blukat <blukatstudios@gmail.com>
2019-11-09 21:41:25 -0500

    fix(refactor): Cleaned up Util files.

--- src/Stats/IStats.cs
+++ src/Stats/IStats.cs
@@ -4,33 +4,15 @@
 using System.Collections.Generic;
 
 public interface IStats {
-
-  // Use this to determine compatible interactions
-  bool HasStat(string stat); // Differentiate legitimate 0 stats from non-existent ones.
+  bool HasStat(string stat);
   List<string> GetStatList();
-
-  // Use this for skills, attributes, perks, modifiers, ability levels, status effects, etc, etc
   int GetStat(string stat);
   void SetStat(string stat, int val);
-
-  // Use this for facts such as name, description
   string GetFact(string fact);
   void SetFact(string fact, string val);
-
-  // Use these for worldly interactions
   bool StatCheck(string stat, int difficulty);
-  
-  /*
-    Use ConsumeStat when an ability requires a certain
-    ammount of a certain stat to be used. (ie magic using mana)
-    will return false instead of creating a deficit
-  */
   bool ConsumeStat(string stat, int amount); 
-
-  /*
-    The caller doesn't care about the outcome of ReceiveDamage.
-  */
   void ReceiveDamage(Damage damage);
-  void RestoreCondition(); // Heal completely, lose status conditions
+  void RestoreCondition();
   void Update(float delta);
 }
